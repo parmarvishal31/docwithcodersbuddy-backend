88bd96afdb282515a6ea2d020a1a3560c9961a5c
DESKTOP-R7P4J37\visha <vishalparmar31320@gmail.com>
2024-01-31 14:33:24 +0530

    register-login-functionality

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from 'cors'
+import connectDb from './db/config.js'
+import authRouter from './routes/auth.js'
 
 dotenv.config()
+
+// database
+connectDb()
 const PORT =process.env.PORT || 8000 
 const app = express();
+app.use(cors())
+app.use(express.json())
 
 app.get('/ping',(req,res)=>{
-    res.status(200).send('<h1>Pong</h1>  {{ Welcome CodewithCoderBuddy - API }}')
+    res.status(200).send('<h1>Pong</h1>  {{ Welcome DocswithCoderBuddy - API }}')
 })
+
+app.use('/api/v1/user',authRouter)
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
